7b63babd2c546185f01e6065f00b9d3ea1bbe7af
Spacie <admin@example.com>
2023-09-22 21:43:27 +0200

    Added subfolder search and compilation for source and include folder (Close #5, Close #6, Close #7), compilation arguments (Close #8), and libs support (WIP #9)

--- make.ps1
+++ make.ps1
@@ -1,12 +1,13 @@
 #Handmade by Galaad Martineaux
 #FORMAT: $folder = '.\path\to\folder'
-$folder_header = '.\headers'
 $folder_include = '.\include'
+$folder_libs = '.\libs'
 $folder_objects = '.\objects'
 $folder_source = '.\src'
 $folder_runtime = '.\runtime'
 $exe_name = 'test.exe' #FORMAT: test.exe
-$compiler = 'g++'
+$compiler = 'g++.exe'
+$compilation_arguments = '-L.\libs -L./libs/libglfw3.a -lglfw3 -lgdi32 -lstdc++ -lopengl32 '
 
 if ($args -contains "-help") {
 	Write-Output "Help - make.ps1 (by Galaad Martineaux)"
@@ -20,17 +21,30 @@ if ($args -contains "-help") {
 #Please refrain from editing further from this
 $currentDirectory = (Get-Location).Path
 
-#STEP 1: FIND EVERY .CPP IN SOURCE FOLDER
-$cppFiles = Get-ChildItem -Path $folder_source -Filter "*.cpp" -File | Resolve-Path -Relative
+
+#STEP 1: FIND EVERY .CPP FILE IN INCLUDE FOLDERS AND SUBFOLDERS
+$cppFiles = Get-ChildItem -Path $folder_include -Filter "*.cpp" -File -Recurse | Resolve-Path -Relative
 $cppFiles = $cppFiles | ForEach-Object {
 	$newFullName = $_.Replace($currentDirectory, ".")
 	New-Object System.IO.FileInfo -ArgumentList $newFullName
 }
+#STEP 2: FIND EVERY SUBFOLDER IN INCLUDE FOLDER, TO INCLUDE THEM
+$subIncludeFolders = Get-ChildItem -Path $folder_include -Directory -Recurse
+$subIncludeFolders = $subIncludeFolders | ForEach-Object {
+    $newFullName = $_.FullName.Replace($currentDirectory, ".")
+    New-Object System.IO.FileInfo -ArgumentList $newFullName
+}
 
-#STEP 2: COMPILE EVERY .CPP FILE IN AN .OBJ OBJECT FILE INTO THE OBJECT FOLDER
+foreach ($sub in $subIncludeFolders) {
+	$folder_include += " -I$sub";
+}
+
+
+
+#STEP 3: COMPILE EVERY .CPP FILE IN AN .OBJ OBJECT FILE INTO THE OBJECT FOLDER
 foreach ($file in $cppFiles) {
 	$outputName = "$folder_objects\" + $file.BaseName + ".obj"
-	$command = "$compiler " + $file + " -I$folder_header -I$folder_include -c -o " + $outputName
+	$command = "$compiler " + $file + " -I$folder_include $compilation_arguments -c -o " + $outputName
 	Write-Output "Compiling $file..."
 	
 	Write-Debug "Command: $command"
@@ -39,15 +53,34 @@ foreach ($file in $cppFiles) {
 	if ($LASTEXITCODE -ne 0) { Exit }
 }
 
-#STEP 3: FIND EVERY .OBJ FILE IN OBJECTS FOLDER
+#STEP 4: FIND EVERY .CPP FILE IN SOURCE FOLDERS AND SUBFOLDERS
+$cppFiles = Get-ChildItem -Path $folder_source -Filter "*.cpp" -File -Recurse | Resolve-Path -Relative
+$cppFiles = $cppFiles | ForEach-Object {
+	$newFullName = $_.Replace($currentDirectory, ".")
+	New-Object System.IO.FileInfo -ArgumentList $newFullName
+}
+
+#STEP 5: COMPILE EVERY .CPP FILE IN AN .OBJ OBJECT FILE INTO THE OBJECT FOLDER
+foreach ($file in $cppFiles) {
+	$outputName = "$folder_objects\" + $file.BaseName + ".obj"
+	$command = "$compiler " + $file + " -I$folder_include $compilation_arguments -c -o " + $outputName
+	Write-Output "Compiling $file..."
+	
+	Write-Debug "Command: $command"
+	
+	Invoke-Expression $command
+	if ($LASTEXITCODE -ne 0) { Exit }
+}
+
+#STEP 6: FIND EVERY .OBJ FILE IN OBJECTS FOLDER
 $objFiles = Get-ChildItem -Path $folder_objects -Filter "*.obj" -File | Resolve-Path -Relative
 $objFiles = $objFiles | ForEach-Object {
 	$newFullName = $_.Replace($currentDirectory, ".")
 	New-Object System.IO.FileInfo -ArgumentList $newFullName
 }
 
-#STEP 4: COMPILE THEM ALL INTO AN EXECUTABLE
-$command = "$compiler -o $folder_runtime\$exe_name"
+#STEP 7: COMPILE THEM ALL INTO AN EXECUTABLE
+$command = "$compiler  -o $folder_runtime\$exe_name $compilation_arguments"
 foreach ($file in $objFiles) {
 	$command += " $file"
 }
